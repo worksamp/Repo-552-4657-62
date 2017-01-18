f10184258b473af38cb96f0e771cd18ca08a2db9
Nikita <work.samp01@gmail.com>
2017-01-18 07:43:19 +0000

    ch1

---
+++ MVCTestDemo/MVCTestDemo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTestDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CheckCountValue(int i)
+        {
+            int x;
+            if (i < 20)
+            {
+                //business logic goes here  
+            }
+            else
+            {
+                throw (new Exception("Out of the Range"));
+            }
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCTestDemo/MVCTestDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTestDemo.Startup))]
+namespace MVCTestDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
